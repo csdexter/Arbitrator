2747c40eeedb1cf9eb5f8240eed88019c0966416
Radu - Eosif Mihailescu <radu.mihailescu@linux360.ro>
2012-07-18 01:32:27 +0100

    BUGFIX: wrong URL in header - fixed pointer to README file

--- Arbitrator.h
+++ Arbitrator.h
@@ -1,7 +1,7 @@
 /* Arduino HAL Library - Bus Arbitration
  * See the README file for author and licensing information. In case it's
  * missing from your distribution, use the one here as the authoritative
- * version: https://github.com/csdexter/HAL/blob/master/README
+ * version: https://github.com/csdexter/Arbitrator/blob/master/README
  *
  * This library deals with accessing busses (SPI, I2C etc.) as common resources,
  * i.e. from more than one place in the code and without causing conflicts.
